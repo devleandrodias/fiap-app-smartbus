5e67a8ef979a1f1f7889ab56f0c1131a099a7279
devleandrodias <leandrodbdias@gmail.com>
2021-09-14 20:28:59 -0300

    adicionando menus em formato de tab e personalizando icones

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import {MapScreen} from './screens/map.screen';
-import {RouteScreen} from './screens/route.screen';
 import {LinesScreen} from './screens/line.screen';
+import {RouteScreen} from './screens/route.screen';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const App = () => {
   const Tab = createBottomTabNavigator();
@@ -12,9 +15,33 @@ const App = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Mapa" component={MapScreen} />
-        <Tab.Screen name="Rota" component={RouteScreen} />
-        <Tab.Screen name="Linhas" component={LinesScreen} />
+        <Tab.Screen
+          name="Mapa"
+          component={MapScreen}
+          options={{
+            tabBarIcon() {
+              return <Icon name="map-marker" size={20} color="#999" />;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Rota"
+          component={RouteScreen}
+          options={{
+            tabBarIcon() {
+              return <Icon name="map-signs" size={20} color="#999" />;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Linhas"
+          component={LinesScreen}
+          options={{
+            tabBarIcon() {
+              return <Icon name="bus" size={20} color="#999" />;
+            },
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
